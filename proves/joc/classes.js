40100d0f28b5b600bd1e06cef635e2bbffaed419
ciber <email@email.cat>
2025-11-04 14:23:24 +0000

    modificacion 04/11/2025 --> juego con todos los niveles

--- proves/joc/classes.js
+++ proves/joc/classes.js
@@ -74,4 +74,82 @@ class Asteroide extends Entitat {
       this.x = pantallaAmple + this.ample;
     }
   }
+  
+
+}
+
+class Laser extends Entitat {
+  constructor(posicio, ample = 10, alt = 4, velocitat = 15) {
+    super(posicio, ample, alt);
+    this.velocitat = velocitat;
+    this.elementHTML.classList.add("laser");
+  }
+
+  moure() {
+    this.x += this.velocitat;
+    if (this.x > pantallaAmple) {
+      this.elementHTML.remove();
+    }
+  }
+}
+class FinalBoss {
+  constructor() {
+    this.x = pantallaAmple - 220;
+    this.y = 100;
+    this.ample = 200;
+    this.alt = 200;
+    this.velocitat = 2;
+    this.vida = 10;
+    this.elementHTML = document.createElement("div");
+    this.elementHTML.classList.add("finalboss");
+    this.elementHTML.style.position = "absolute";
+    this.elementHTML.style.width = "200px";
+    this.elementHTML.style.height = "200px";
+    this.elementHTML.style.backgroundImage = 'url("/img/finalboss.png")';
+    this.elementHTML.style.backgroundSize = "cover";
+    pantalla.append(this.elementHTML);
+  }
+
+  dibuixar() {
+    this.elementHTML.style.left = `${this.x}px`;
+    this.elementHTML.style.top = `${this.y}px`;
+  }
+
+  moure() {
+    this.y += this.velocitat;
+    if (this.y <= 0 || this.y + this.alt >= pantallaAlt) {
+      this.velocitat *= -1;
+    }
+  }
+
+  disparar() {
+    const laser = new LaserBoss({ x: this.x, y: this.y + this.alt / 2 - 5 });
+    vectorLasersBoss.push(laser);
+    pantalla.append(laser.elementHTML);
+  }
+}
+class LaserBoss {
+  constructor(posicio) {
+    this.x = posicio.x;
+    this.y = posicio.y;
+    this.velocitat = 5;
+    this.ample = 10;
+    this.alt = 10;
+    this.elementHTML = document.createElement("div");
+    this.elementHTML.classList.add("laserboss");
+    this.elementHTML.style.position = "absolute";
+    this.elementHTML.style.width = "10px";
+    this.elementHTML.style.height = "10px";
+    this.elementHTML.style.backgroundColor = "purple";
+    pantalla.append(this.elementHTML);
+  }
+
+  dibuixar() {
+    this.elementHTML.style.left = `${this.x}px`;
+    this.elementHTML.style.top = `${this.y}px`;
+  }
+
+  moure() {
+    this.x -= this.velocitat;
+  }
 }
--- proves/joc/main.js
+++ proves/joc/main.js
@@ -1,93 +1,262 @@
-// (main.js) Noves variables de pantalla
+// Variables de pantalla
 const pantallaAmple = window.innerWidth;
 const pantallaAlt = window.innerHeight;
 const fotogrames = 1000 / 60;
 
-// (main.js) Variables per la gestió de la partida
-const nivell = 1;
-const maxPunts = 100;
+// Variables de partida
+let nivell = 1;
+let finalBoss = null;
 const vectorAsteroides = [];
-const vectorEnemics = [];
-const maxAsteroides = 100;
-const maxEnemics = 12;
+const vectorLasers = [];
+const vectorLasersBoss = [];
+const maxAsteroides = 20;
 
-// --------- Pantalla del Joc ---------
 const pantalla = document.querySelector("#pantalla");
 const infoPartida = document.querySelector("#infoPartida");
 
-// (main.js) Actualització: Instanciació de Classes (Jugador, Enemic i Asteroides)
-
-// ---- Objecte Jugador ----
-// Constructor: nom, vides, velocitat, posicio, ample, alt
+// Jugador
 const jugador = new Jugador("Pepito", 3, 10, {x: 100, y: 300}, 150, 100);
 pantalla.append(jugador.elementHTML);
 
-// ---- Vector d'objectes Enemic ----
-for (let i = 0; i < maxEnemics; i++) {
-  // Fem un enemic de 50x50
-  let posX = pantallaAmple + 50;
-  let posY = Math.floor(Math.random() * (pantallaAlt - 50));
-  let velocitat = Math.floor(Math.random() * 5) + 1;
-  vectorEnemics.push(new Enemic(velocitat, {x: posX, y: posY}, 50, 50));
-  pantalla.append(vectorEnemics[i].elementHTML);
-}
-
-// ---- Vector d'objectes Asteroides ----
+// Asteroides inicials
 for (let i = 0; i < maxAsteroides; i++) {
-  // Fem un asteroide de 5x5
   let posX = Math.floor(Math.random() * pantallaAmple - 3);
   let posY = Math.floor(Math.random() * pantallaAlt - 3);
-  let velocitat = Math.floor(Math.random() * 10) + 1;
-  vectorAsteroides.push(new Asteroide(velocitat, {x: posX, y: posY}, 3, 3));
+  let velocitat = Math.random() * 1.5 + 0.2;
+  vectorAsteroides.push(new Asteroide(velocitat, {x: posX, y: posY}, 30, 30));
   pantalla.append(vectorAsteroides[i].elementHTML);
 }
 
-// (main.js) Actualització de les dades d'informació de la partida
-// ------- Informació de la partida -------
+// Marcador
 const elementNom = document.createElement("p");
 const elementPunts = document.createElement("p");
 const elementDerribats = document.createElement("p");
 const elementVides = document.createElement("p");
 elementNom.innerHTML = `Jugador: ${jugador.nom}`;
-infoPartida.append(elementNom);
 elementPunts.innerHTML = `Punts: ${jugador.punts}`;
-infoPartida.append(elementPunts);
 elementDerribats.innerHTML = `Kills: ${jugador.derribats}`;
-infoPartida.append(elementDerribats);
 elementVides.innerHTML = `Vides: ${jugador.vides}`;
-infoPartida.append(elementVides);
+infoPartida.append(elementNom, elementPunts, elementDerribats, elementVides);
 
-
-// (main.js) Esdeveniments de teclat
-// Control de la nau del jugador quan prem una tecla
+// Teclat
 window.addEventListener("keydown", (event) => {
   switch(event.code) {
     case "ArrowUp":
       jugador.y -= jugador.velocitat;
+      jugador.dibuixar();
       break;
     case "ArrowDown":
       jugador.y += jugador.velocitat;
+      jugador.dibuixar();
       break;
-    default:
+    case "Space":
+      const laser = new Laser(
+        { x: jugador.x + jugador.ample, y: jugador.y + jugador.alt / 2 - 3 }
+      );
+      vectorLasers.push(laser);
+      pantalla.append(laser.elementHTML);
       break;
   }
 });
 
-// (main.js) Bucle d'animació del joc
+// Funció de col·lisió
+function colisiona(a, b) {
+  return (
+    a.x < b.x + b.ample &&
+    a.x + a.ample > b.x &&
+    a.y < b.y + b.alt &&
+    a.y + a.alt > b.y
+  );
+}
+
+// Funció per passar de nivell
+function passarDeNivell() {
+  nivell += 1;
+  console.log(`🚀 Passant al nivell ${nivell}`);
+
+  vectorAsteroides.forEach(asteroide => {
+    if (nivell === 2) {
+      asteroide.elementHTML.style.backgroundImage = 'url("/img/fireball2.png")';
+      asteroide.velocitat += 1;
+      asteroide.ample = 50;
+      asteroide.alt = 50;
+      asteroide.elementHTML.style.width = "50px";
+      asteroide.elementHTML.style.height = "50px";
+    } else if (nivell === 3) {
+      asteroide.elementHTML.style.backgroundImage = 'url("/img/misilalien.gif")';
+      asteroide.velocitat += 1.2;
+      asteroide.ample = 100;
+      asteroide.alt = 100;
+      asteroide.elementHTML.style.width = "100px";
+      asteroide.elementHTML.style.height = "100px";
+    }
+  });
+
+  const missatge = document.createElement("div");
+  missatge.innerText = `Nivell ${nivell}`;
+  missatge.style.position = "absolute";
+  missatge.style.top = "50%";
+  missatge.style.left = "50%";
+  missatge.style.transform = "translate(-50%, -50%)";
+  missatge.style.color = "white";
+  missatge.style.fontSize = "40px";
+  missatge.style.zIndex = "100";
+  pantalla.append(missatge);
+  setTimeout(() => missatge.remove(), 2000);
+}
+
+// Missatge de victòria
+function mostrarVictoria() {
+  const win = document.createElement("div");
+  win.innerText = "🎉 YOU WIN 🎉";
+  win.style.position = "absolute";
+  win.style.top = "50%";
+  win.style.left = "50%";
+  win.style.transform = "translate(-50%, -50%)";
+  win.style.color = "lime";
+  win.style.fontSize = "60px";
+  win.style.zIndex = "200";
+  pantalla.append(win);
+}
+
+// Bucle del joc
 setInterval(() => {
-  // 1. Gestió del jugador
   jugador.dibuixar();
   jugador.moure();
 
-  // 2. Gestió dels enemics
-  vectorEnemics.forEach(enemic => {
-    enemic.dibuixar();
-    enemic.moure();
-  });
-
-  // 3. Gestió dels asteroides
   vectorAsteroides.forEach(asteroide => {
     asteroide.dibuixar();
     asteroide.moure();
   });
+
+  // Regenerar asteroides si queden 10 o menys
+  if (vectorAsteroides.length <= 10 && !finalBoss) {
+    for (let i = 0; i < 5; i++) {
+      let posX = pantallaAmple + Math.floor(Math.random() * 100);
+      let posY = Math.floor(Math.random() * pantallaAlt - 3);
+      let velocitat = Math.random() * 1.5 + 0.2;
+
+      if (nivell === 3) {
+        velocitat += 2;
+        const nouAsteroide = new Asteroide(velocitat, {x: posX, y: posY}, 100, 100);
+        nouAsteroide.elementHTML.style.backgroundImage = 'url("/img/misilalien.gif")';
+        nouAsteroide.elementHTML.style.width = "100px";
+        nouAsteroide.elementHTML.style.height = "100px";
+        pantalla.append(nouAsteroide.elementHTML);
+        vectorAsteroides.push(nouAsteroide);
+      } else if (nivell === 2) {
+        velocitat += 1;
+        const nouAsteroide = new Asteroide(velocitat, {x: posX, y: posY}, 50, 50);
+        nouAsteroide.elementHTML.style.backgroundImage = 'url("/img/fireball2.png")';
+        nouAsteroide.elementHTML.style.width = "50px";
+        nouAsteroide.elementHTML.style.height = "50px";
+        pantalla.append(nouAsteroide.elementHTML);
+        vectorAsteroides.push(nouAsteroide);
+      } else {
+        const nouAsteroide = new Asteroide(velocitat, {x: posX, y: posY}, 30, 30);
+        pantalla.append(nouAsteroide.elementHTML);
+        vectorAsteroides.push(nouAsteroide);
+      }
+    }
+  }
+
+  // Boss final
+  if (finalBoss) {
+    finalBoss.moure();
+    finalBoss.dibuixar();
+    if (Math.random() < 0.02) {
+      finalBoss.disparar();
+    }
+  }
+
+  // Làsers del boss
+  for (let i = vectorLasersBoss.length - 1; i >= 0; i--) {
+    const laser = vectorLasersBoss[i];
+    laser.moure();
+    laser.dibuixar();
+
+    if (colisiona(laser, jugador)) {
+      alert("Has sido destruido por el jefe final 💀");
+      location.reload();
+    }
+
+    if (laser.x < 0) {
+      laser.elementHTML.remove();
+      vectorLasersBoss.splice(i, 1);
+    }
+  }
+
+  // Col·lisions amb el jugador
+  for (let i = vectorAsteroides.length - 1; i >= 0; i--) {
+    const asteroide = vectorAsteroides[i];
+
+    if (colisiona(jugador, asteroide)) {
+      asteroide.elementHTML.remove();
+      vectorAsteroides.splice(i, 1);
+
+      jugador.vides -= 1;
+      elementVides.innerHTML = `Vides: ${jugador.vides}`;
+
+      jugador.elementHTML.style.filter = "brightness(0.5)";
+      setTimeout(() => {
+        jugador.elementHTML.style.filter = "none";
+      }, 300);
+
+      if (jugador.vides <= 0) {
+        alert("Game Over!");
+        location.reload();
+      }
+    }
+  }
+
+  // Làsers del jugador
+  for (let i = vectorLasers.length - 1; i >= 0; i--) {
+    const laser = vectorLasers[i];
+    laser.dibuixar();
+    laser.moure();
+
+    // Colisión con el jefe final
+    if (finalBoss && colisiona(laser, finalBoss)) {
+      laser.elementHTML.remove();
+      vectorLasers.splice(i, 1);
+      finalBoss.vida -= 1;
+
+      if (finalBoss.vida <= 0) {
+        finalBoss.elementHTML.remove();
+        finalBoss = null;
+        mostrarVictoria();
+      }
+
+      continue;
+    }
+
+    // Colisión con asteroides
+    for (let j = vectorAsteroides.length - 1; j >= 0; j--) {
+      const asteroide = vectorAsteroides[j];
+
+      if (colisiona(laser, asteroide)) {
+        asteroide.elementHTML.remove();
+        laser.elementHTML.remove();
+        vectorAsteroides.splice(j, 1);
+        vectorLasers.splice(i, 1);
+        jugador.punts += 10;
+        jugador.derribats += 1;
+        elementPunts.innerHTML = `Punts: ${jugador.punts}`;
+        elementDerribats.innerHTML = `Kills: ${jugador.derribats}`;
+
+        if (jugador.derribats >= 10 && nivell === 1) {
+          passarDeNivell();
+        }
+        if (jugador.derribats >= 25 && nivell === 2) {
+          passarDeNivell();
+        }
+        if (jugador.derribats >= 40 && nivell === 3 && !finalBoss) {
+          nivell += 1;
+          finalBoss = new FinalBoss();
+        }
+
+        break;
+      }
+    }
+  }
 }, fotogrames);
